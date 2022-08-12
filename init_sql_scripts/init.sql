52ebaf5a85970c50b147aa3c45ce3f98016f9243
osaid almaymani <oamaymani@gmail.com>
2022-08-12 20:38:23 +0300

    a

---
+++ init_sql_scripts/init.sql
@@ -0,0 +1,6 @@
+create table english2arabic (country text,capital text);
+
+insert into english2arabic(country, capital) values ('India', 'Delhi');
+insert into english2arabic(country, capital) values ('China', 'Beijing');
+insert into english2arabic(country, capital) values ('Bhutan', 'Thimpu');
+insert into english2arabic(country, capital) values ('Russia', 'Moscow');
